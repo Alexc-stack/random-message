aad0800a14332c2a332ee0acbf9023a555b12b3c
Darkcosmic <carteralexander011@gmail.com>
2023-06-04 19:57:08 -0400

    finishing up random messages an random reps

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-// this program is to make random messages basketball training messages
+// this program is to make random basketball training messages
 
 // making the object to hold the arrays of data
 
 const arrHolder = {
   catagory: ['Dribbling', 'Shooting', 'Passing'],
   reps: [10, 30, 50],
-  messages: {
-    Dribbling: ['pound dribble', 'in an out', 'crossover', 'behind the back', 'between the legs'],
-    Shooting: ['freethrows', 'threepointer', 'stepback', 'midrange', 'fadeaway'],
-    Passing: ['bounce pass', 'lob pass', 'bullet pass', 'no look pass'],
-  },
+  Dribbling: ['pound dribble', 'in an out', 'crossover', 'behind the back', 'between the legs'],
+  Shooting: ['freethrows', 'threepointer', 'stepback', 'midrange', 'fadeaway'],
+  Passing: ['bounce pass', 'lob pass', 'bullet pass', 'no look pass'],
 }
 
-// this generates a random peice of data, the catagorey an the reps
+// a function that generates both the catagory a matches it with the random message
 
-const ranCatagory = () => {
-    let ranIndex = Math.floor(Math.random() * 3);
-    let item = arrHolder.catagory[ranIndex]
-    return item
+const randomMessage = () => {
+    let ranIndex = Math.floor(Math.random() * arrHolder.catagory.length) 
+    let ranCat = arrHolder.catagory[ranIndex];
+    if (ranCat === 'Dribbling') {
+        let ranDribble = Math.floor(Math.random() * arrHolder.Dribbling.length)
+        return `${ranCat}: ${arrHolder.Dribbling[ranDribble]}`
+    } else if (ranCat === 'Shooting') {
+        let ranShoot = Math.floor(Math.random() * arrHolder.Shooting.length)
+        return `${ranCat}: ${arrHolder.Shooting[ranShoot]}`
+    } else if (ranCat === "Passing") {
+        let ranPass = Math.floor(Math.random() * arrHolder.Passing.length)
+        return `${ranCat}: ${arrHolder.Passing[ranPass]}`
+    }
 }
-
+//Combines the random reps with the random messages
 const ranReps = () => {
-    let ranIndex = Math.floor(Math.random() * 3);
-    let item = arrHolder.reps[ranIndex]
-    return item
+    let ranNum = Math.floor(Math.random() * arrHolder.reps.length)
+    let reps = arrHolder.reps[ranNum]
+    return `${randomMessage()} for ${reps}X`
 }
-
-// now depending on what catagory pops up we need that message to populate
-
 console.log(ranReps())
+
+
+
