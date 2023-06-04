dab66679fa9a2cbe36484f160f9a6fc7772865ba
Darkcosmic <carteralexander011@gmail.com>
2023-06-04 11:33:12 -0400

    adding the data

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // this program is to make random messages
+
+// a place where the random messages is held
+let catagory = '';
+let message = '';
+let reps = '';
+
+// making the object to hold the arrays of data
+
+const arrHolder = {
+  catagory: ['Dribbling', 'Shooting', 'Passing'],
+  reps: [10, 30, 50],
+  messages: {
+    Dribbling: ['pound dribble', 'in an out', 'crossover', 'behind the back', 'between the legs'],
+    Shooting: ['freethrows', 'threepointer', 'stepback', 'midrange', 'fadeaway'],
+    Passing: ['bounce pass', 'lob pass', 'bullet pass', 'no look pass'],
+  },
+}
